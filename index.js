02238ef28f456f86d51282a02df9f3317b066827
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-26 17:39:47 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ module.exports = defence
 var commonmark = require('commonmark')
 var EOL = require('os').EOL
 
-function defence(markup, infoStrings) {
+function defence (markup, infoStrings) {
   var enteringMatchingFencedCode = (
-    infoStrings === undefined ?
-      enteringFencedCodeBlock :
-      function(event, node) {
-        return (
-          enteringFencedCodeBlock(event, node) &&
-          infoStrings.some(function(permitted) {
-            return permitted === node.info }) ) } )
-  var walker = new commonmark.Parser().parse(markup).walker()
+    infoStrings === undefined
+    ? enteringFencedCodeBlock
+    : function (event, node) {
+      return (
+        enteringFencedCodeBlock(event, node) &&
+        infoStrings.some(function (permitted) {
+          return permitted === node.info
+        })
+      )
+    }
+  )
+  var walker = new commonmark.Parser()
+  .parse(markup)
+  .walker()
   var event, node, startLine, endLine
   var lastBlockEndedOnLine = 1
   var output = ''
@@ -23,30 +29,41 @@ function defence(markup, infoStrings) {
       startLine = firstLineOf(node)
       endLine = lastLineOf(node)
       if (startLine > lastBlockEndedOnLine) {
-        output += newlines(( startLine - lastBlockEndedOnLine + 1 )) }
+        output += newlines(startLine - lastBlockEndedOnLine + 1)
+      }
       lastBlockEndedOnLine = endLine
-      output += node.literal }
-    else if (endingDocument(event, node)) {
+      output += node.literal
+    } else if (endingDocument(event, node)) {
       var lastLineOfDocument = lastLineOf(node)
-      output += newlines(lastLineOfDocument - lastBlockEndedOnLine + 1) }
-    event = walker.next() }
-  return output }
+      output += newlines(lastLineOfDocument - lastBlockEndedOnLine + 1)
+    }
+    event = walker.next()
+  }
+  return output
+}
 
-function enteringFencedCodeBlock(event, node) {
+function enteringFencedCodeBlock (event, node) {
   return (
     event.entering === true &&
-    node.type === 'code_block' ) }
+    node.type === 'code_block'
+  )
+}
 
-function endingDocument(event, node) {
+function endingDocument (event, node) {
   return (
     event.entering === false &&
-    node.type === 'document' ) }
+    node.type === 'document'
+  )
+}
 
-function lastLineOf(node) {
-  return node.sourcepos[1][0] }
+function lastLineOf (node) {
+  return node.sourcepos[1][0]
+}
 
-function firstLineOf(node) {
-  return node.sourcepos[0][0] }
+function firstLineOf (node) {
+  return node.sourcepos[0][0]
+}
 
-function newlines(number) {
-  return new Array(number + 1).join(EOL) }
+function newlines (number) {
+  return new Array(number + 1).join(EOL)
+}
--- test.js
+++ test.js
@@ -4,26 +4,32 @@ var fs = require('fs')
 var glob = require('glob')
 var path = require('path')
 
-tape.test(function(test) {
-  glob.sync('tests/*')
-    .forEach(function(directory) {
-      test.equal(
-        defence(
-          read(path.join(directory, 'input')),
-          readJSON(path.join(directory, 'infostrings'))),
-        read(path.join(directory, 'output')),
-        directory) })
-  test.end() })
+tape.test(function (test) {
+  glob.sync('tests/*').forEach(function (directory) {
+    test.equal(
+      defence(
+        read(path.join(directory, 'input')),
+        readJSON(path.join(directory, 'infostrings'))
+      ),
+      read(path.join(directory, 'output')),
+      directory)
+  })
+  test.end()
+})
 
-function read(file) {
+function read (file) {
   try {
-    return fs.readFileSync(file).toString() }
-  catch (e) {
-    return undefined } }
+    return fs.readFileSync(file).toString()
+  } catch (e) {
+    return undefined
+  }
+}
 
-function readJSON(file) {
+function readJSON (file) {
   try {
     var input = fs.readFileSync(file).toString()
-    return JSON.parse(input) }
-  catch (e) {
-    return undefined } }
+    return JSON.parse(input)
+  } catch (e) {
+    return undefined
+  }
+}
